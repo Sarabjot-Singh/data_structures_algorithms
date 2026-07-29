926adfe74d31625f2a562f953f790ec43cfd933a
Sarabjot Singh <sarabjotsingh@Sarabjots-MacBook-Pro.local>
2026-07-29 22:48:31 +0530

    Add: graph->flood_fill

--- graphs/flood_fill.py
+++ graphs/flood_fill.py
@@ -48,3 +48,61 @@
 # 0 <= image[i][j], color < 216
 # 0 <= sr < m
 # 0 <= sc < n
+
+
+class Solution:
+
+    def is_valid(self, row, col, n, m):
+        if row < 0 or row >= n or col < 0 or col >= m:
+            return False
+        return True
+
+    def dfs(self, image, visited, n, m, sr, sc, color, original_color):
+        image[sr][sc] = color
+        visited[sr][sc] = 1
+
+        # helper to create neighbour indices
+        row_iterator = [-1, 1, 0, 0]
+        col_iterator = [0, 0, -1, 1]
+
+        for k in range(len(row_iterator)):
+            # create indices to neighbours
+            row = sr + row_iterator[k]
+            col = sc + col_iterator[k]
+
+            # check if all conditions for flood fill are valid or not
+            if self.is_valid(row, col, n, m) and visited[row][col] == 0 and image[row][col] == original_color:
+                # perform dfs of neighbours
+                self.dfs(image, visited, n, m, row, col, color, original_color)
+
+    def flood_fill(self, image, sr, sc, color):
+        n = len(image)
+        m = len(image[0])
+
+        visited = []
+        for _ in range(n):
+            row = []
+            for _ in range(m):
+                row.append(0)
+            visited.append(row)
+
+        original_color = image[sr][sc]
+
+        # start dfs from image[sr][sc]
+        self.dfs(image, visited, n, m, sr, sc, color, original_color)
+
+        return image
+    
+
+image = [[1,1,1],[1,1,0],[1,0,1]]
+sr = 1
+sc = 1
+color = 2
+
+image = [[0,0,0],[0,0,0]]
+sr = 0
+sc = 0
+color = 0
+
+sol_obj = Solution()
+print(sol_obj.flood_fill(image, sr, sc, color))
